aeac0bfe3608ce3a53882ccb403cde50caacbf22
CatherineZM <49298813+CatherineZM@users.noreply.github.com>
2023-02-17 22:35:56 -0500

    Finished capstone homepage and initialized other pages

--- src/Components/Capstone/MainPage.js
+++ src/Components/Capstone/MainPage.js
@@ -2,32 +2,41 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../Capstone/styles/MainPage.css';
+import '../Capstone/styles/SideBar.css';
 
 import { Container, AppBar, Toolbar, Box, IconButton, Drawer, 
-  List, ListItemButton, ListItemText, Divider, ListItem, Avatar} 
+  List, ListItemButton, ListItemText, ListItemIcon, Divider, ListItem, ListSubheader, ListItemAvatar, Avatar} 
   from '@mui/material';
 
 import MenuIcon from '@mui/icons-material/Menu';
-import PersonIcon from '@mui/icons-material/Person';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import HomeIcon from '@mui/icons-material/Home';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import ChatIcon from '@mui/icons-material/Chat';
+import InfoIcon from '@mui/icons-material/Info';
 
 import Banner from './Components/Banner.component';
 
 const MenuItems = [
   {
     Label: "Home",
-    link: "/value"
+    link: "/value",
+    icon: <HomeIcon />
   },
   {
     Label: "List of Values",
-    link: "/value/list"
+    link: "/value/list",
+    icon: <ListAltIcon />
   },
   {
     Label: "Demo",
-    link: "/value/demo"
+    link: "/value/demo",
+    icon: <ChatIcon />
   },
   {
     Label: "About Us",
-    link: "/value/about"
+    link: "/value/about",
+    icon: <InfoIcon />
   }
 ]
 
@@ -51,6 +60,7 @@ class Capstone extends React.Component {
       <List>
         {MenuItems.map((menuItem, index) => (
           <ListItemButton key={index} href={menuItem.link}>
+            <ListItemIcon> {menuItem.icon} </ListItemIcon>
             <ListItemText primary={menuItem.Label} />
           </ListItemButton>
         ))}
@@ -68,44 +78,49 @@ class Capstone extends React.Component {
                             aria-label="menu" sx={{ mr: 2 }} onClick={this.onClickMenu}>
                   <MenuIcon />
                 </IconButton>
-                <Drawer open={this.state.open} anchor="left" onClose={this.onClickMenu}>
+                <Drawer className="SideBar" open={this.state.open} anchor="left" onClose={this.onClickMenu}>
+                  <div className='Close'>
+                    <IconButton onClick={this.onClickMenu}>
+                      <ArrowBackIosNewIcon />
+                    </IconButton>
+                  </div>
+                  <Divider />
                   {this.MenuList()}
                   <Divider />
                   <List>
-                    <ListItem>
-                      <ListItemText primary="Attribution:"/>
-                    </ListItem>
-                    <ListItem>
-                      <ListItemText primary="Team 2022518 Members:"/>
-                    </ListItem>
-                    <ListItem>
-                      <ListItemText primary="Boyuan Shao"
-                    secondary="UofT ECE2T2+PEY"/>
-                    </ListItem>
-                    <ListItem>
-                      <ListItemText primary="Mo Zhou"
-                    secondary="UofT ECE2T2+PEY"/>
-                    </ListItem>
-                    <ListItem>
-                      <ListItemText primary="Qiyan Liu"
-                    secondary="UofT ECE2T2+PEY"/>
-                    </ListItem>
-                    <ListItem>
-                      <ListItemText primary="Yonghai Yu"
-                    secondary="UofT ECE2T2+PEY"/>
-                    </ListItem>
-                    <ListItem>
-                      <ListItemText primary="Special Thanks to:"/>
-                    </ListItem>
-                    <ListItem>
-                      <ListItemText primary="Prof. Jonathan Rose"
-                      secondary="Supervisor"/>
-                    </ListItem>
-                    <ListItem>
-                      <ListItemText primary="Inci McGreal"
-                      secondary="Administrator"/>
-                    </ListItem>
+                    <ListSubheader>University of Toronto - Capstone 2022</ListSubheader>
+                    <ListSubheader>Team 2022518</ListSubheader>
                   </List>
+                  {/* <Divider />
+                  <List>
+                    <ListSubheader>Attribution:</ListSubheader>
+                    <ListSubheader>Team 2022518 Members:</ListSubheader>
+                    <ListItem>
+                      <ListItemAvatar><Avatar><PersonIcon /></Avatar></ListItemAvatar>
+                      <ListItemText primary="Boyuan Shao" secondary="UofT ECE2T2+PEY"/>
+                    </ListItem>
+                    <ListItem>
+                      <ListItemAvatar><Avatar><PersonIcon /></Avatar></ListItemAvatar>
+                      <ListItemText primary="Mo Zhou" secondary="UofT ECE2T2+PEY"/>
+                    </ListItem>
+                    <ListItem>
+                      <ListItemAvatar><Avatar><PersonIcon /></Avatar></ListItemAvatar>
+                      <ListItemText primary="Qiyan Liu" secondary="UofT ECE2T2+PEY"/>
+                    </ListItem>
+                    <ListItem>
+                      <ListItemAvatar><Avatar><PersonIcon /></Avatar></ListItemAvatar>
+                      <ListItemText primary="Yonghai Yu" secondary="UofT ECE2T2+PEY"/>
+                    </ListItem>
+                    <ListSubheader>Special Thanks to:</ListSubheader>
+                    <ListItem>
+                      <ListItemAvatar><Avatar><PersonIcon /></Avatar></ListItemAvatar>
+                      <ListItemText primary="Prof. Jonathan Rose" secondary="Supervisor"/>
+                    </ListItem>
+                    <ListItem>
+                      <ListItemAvatar><Avatar><PersonIcon /></Avatar></ListItemAvatar>
+                      <ListItemText primary="Ms. Inci McGreal" secondary="Administrator"/>
+                    </ListItem>
+                  </List> */}
                 </Drawer>
               </Toolbar>
             </AppBar>
